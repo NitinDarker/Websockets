1219377e6d3d351032f78fa5f7ce5ef59b036330
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-04-18 00:13:19 +0530

    receive a message

--- 01_ws/src/index.ts
+++ 01_ws/src/index.ts
@@ -1,11 +1,18 @@
-import { CLOSING, WebSocketServer } from "ws";
+import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8000 });
 
+// On WebSocket connection, call this function
 wss.on("connection", (socket) => {
     console.log("Connection established!");
     socket.send("Hello World");
+    
     setInterval(() => {
         socket.send("Current price of Solana is: " + Math.random());
     }, 2000);
+    
+    socket.on("message", (e) => {
+        console.log(e.toString());
+    })
+
 })
