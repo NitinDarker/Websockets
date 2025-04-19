d825b0ae5bad8d094d4bcc80fa7b55b3d74c462f
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-04-19 19:10:39 +0530

    making frontend of chat app

--- 03_Real-Time-Chat/frontend/src/App.tsx
+++ 03_Real-Time-Chat/frontend/src/App.tsx
@@ -1,18 +1,44 @@
-import { useEffect } from 'react'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [socket, setSocket] = useState<WebSocket | null>();
+  const [broadcast, setBroadcast] = useState<string>("");
+  const broadcastRef = useRef<HTMLInputElement>(null);
 
-    useEffect(() => {
-        const ws = new WebSocket("ws://localhost:8000");
-        console.log(ws);
-    }, []); 
+  useEffect(() => {
+    const ws: WebSocket = new WebSocket("ws://localhost:8000");
+    setSocket(ws);
+    ws.onopen = () => {
+      console.log("A Connection to server is established!");
+      ws.send("From Client: Greetings!");
+    };
+
+    ws.onmessage = (event) => {
+      console.log("From Server: " + event.data);
+      setBroadcast((msg) => {
+        return event.data + "\n" + msg;
+      });
+    };
+  }, []);
+
+  function communicate() {
+    if (broadcastRef.current) {
+      socket?.send(broadcastRef.current.value);
+    }
+  }
 
   return (
     <>
-      
+      <input
+        ref={broadcastRef}
+        placeholder="Enter a message"
+        type="text"
+      ></input>
+      <button onClick={communicate}>Send</button>
+      <pre id="broadcastBox">{broadcast}</pre>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
