65f377f80ef880cfdccb96f7e968d65bbe447c1d
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-04-19 18:56:14 +0530

    in the middle of building a chat app

--- 03_Real-Time-Chat/backend/src/index.ts
+++ 03_Real-Time-Chat/backend/src/index.ts
@@ -3,7 +3,6 @@ const wss = new WebSocketServer({ port: 8000 });
 
 function sendMessage(socket: WebSocket, broadcast: string) {
   for (let s of socketList) {
-    if (s === socket) continue;
     s.send(broadcast);
   }
 }
@@ -11,16 +10,19 @@ function sendMessage(socket: WebSocket, broadcast: string) {
 let socketList: WebSocket[] = [];
 
 wss.on("connection", (socket) => {
-    console.log("A connection is established!");
-    socketList.push(socket);
-    socket.send("Hey there!");
+  console.log("A connection is established!");
+  socketList.push(socket);
+  socket.send("From Server: Hey there!");
 
-    socket.on("message", (event) => {
-        console.log("Response: " + event.toString());
-        sendMessage(socket, event.toString());
-    })
+  socket.on("message", (event) => {
+    console.log("Response: " + event.toString());
+    sendMessage(socket, event.toString());
+  });
 
-    socket.on("close", () => {
-        console.log("Connection lost!");
-    })
-})
+  socket.on("close", () => {
+    console.log("A Connection is lost!");
+    socketList = socketList.filter((s) => {
+      return s != socket;
+    });
+  });
+});
